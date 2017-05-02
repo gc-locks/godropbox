84159af47bcb19a131f067adf29e8caf8dab37b6
Tatsuji Tsuchiya <ta2xeo@gmail.com>
2017-05-02 15:30:54 +0900

    追加したテストが通らない問題の修正

--- table_test.go
+++ table_test.go
@@ -209,22 +209,16 @@ func (s *TableSuite) TestNestedRightJoin(c *gc.C) {
 }
 
 func (s *TableSuite) TestAlias(c *gc.C) {
-	t2, err := table2.Alias("a1")
-	if err != nil {
-		c.Fatal("error occurred")
-	}
+	t2 := table2.Alias("a1")
 
-	t3, err := table3.Alias("a2")
-	if err != nil {
-		c.Fatal("error occurred")
-	}
+	t3 := table3.Alias("a2")
 
 	join1 := table1.InnerJoinOn(t2, Eq(table1Col3, t2.columns[0]))
 	join2 := join1.RightJoinOn(t3, Eq(table1Col1, t3.columns[0]))
 
 	buf := &bytes.Buffer{}
 
-	err = join2.SerializeSql("db", buf)
+	err := join2.SerializeSql("db", buf)
 	c.Assert(err, gc.IsNil)
 
 	sql := buf.String()
